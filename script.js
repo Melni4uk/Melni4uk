f2eef0e80abcf714a5ae249fe7bba123e408d083
Melni4uk <nick.melni4uk@gmail.com>
2016-10-05 15:54:27 +0300

    Chenged JS file

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-var store = {"name": "NOVUS", "address": "Schevchenka str. 300", "employeesCount": "40"}
+/*var store = {"name": "NOVUS", "address": "Schevchenka str. 300", "employeesCount": "40"}
 localStorage.setItem('store', JSON.stringify(store));
 var retrivedObject = localStorage.getItem('store');
-console.log('retrivedObject: ' , JSON.parse(retrivedObject));
+console.log('retrivedObject: ' , JSON.parse(retrivedObject));*/
+
+/*
+var getInfo = document.getElementById('informatiomForm')
+form.onsubmit  = function(event){
+    event.preventDefault();
+    console.log(form.adress);
+};*/
+function processForm() {console.log("Hello")}
